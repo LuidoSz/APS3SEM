aad2b880d2fc9871411b33bd2753468d05ba8886
OMirandinha <mirandavitorhugo25@gmail.com>
2024-04-18 10:37:43 -0300

    botão de iniciar funcionando agora
    fiz algumas alterações no código e agora o botão de iniciar abre o questionário principal.

---
+++ TotemMuseu/src/totemmuseu/frmPrincipal.java
@@ -0,0 +1,132 @@
+
+package totemmuseu;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public abstract class frmPrincipal extends javax.swing.JDialog implements ActionListener{
+
+    
+    frmPrincipal(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        
+    }
+    
+ 
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        btn_Iniciar = new javax.swing.JButton();
+        lbl_Cabecario = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(1920, 1280));
+        setSize(new java.awt.Dimension(10, 10));
+
+        btn_Iniciar.setFont(new java.awt.Font("Times New Roman", 0, 48)); // NOI18N
+        btn_Iniciar.setText("INICIAR QUESTIONÁRIO");
+        btn_Iniciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_IniciarActionPerformed(evt);
+            }
+        });
+
+        lbl_Cabecario.setBackground(new java.awt.Color(153, 153, 153));
+        lbl_Cabecario.setFont(lbl_Cabecario.getFont().deriveFont(lbl_Cabecario.getFont().getStyle() | java.awt.Font.BOLD, lbl_Cabecario.getFont().getSize()+38));
+        lbl_Cabecario.setText("QUESTIONÁRIO (TEXTO PRA ENCHER LINGUÇA)");
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel2.setLabelFor(jLabel2);
+        jLabel2.setText("dfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsfsfsfsffsfsdfsdfdsf\\sf\\sfdfsdfdsf\\sf\\sf");
+        jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jLabel2.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 103, Short.MAX_VALUE)
+                        .addComponent(lbl_Cabecario, javax.swing.GroupLayout.PREFERRED_SIZE, 1172, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(123, 123, 123)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(338, 338, 338)
+                                .addComponent(btn_Iniciar, javax.swing.GroupLayout.PREFERRED_SIZE, 599, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(124, 124, 124)))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(11, 11, 11)
+                .addComponent(lbl_Cabecario, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addGap(456, 456, 456)
+                .addComponent(btn_Iniciar, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(72, 72, 72))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+//método que faz o botão abrir uma nova janela/interface
+    private void btn_IniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_IniciarActionPerformed
+
+                frmQuestionario questionario = new frmQuestionario(new javax.swing.JFrame(), true);
+   
+                questionario.setVisible(true);
+                
+
+    
+    }//GEN-LAST:event_btn_IniciarActionPerformed
+
+    
+    
+            
+     
+
+    
+
+       
+    public static void main(String args[]) {
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                frmPrincipal dialog = new frmPrincipal(new javax.swing.JFrame(), true) {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+                };
+                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_Iniciar;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel lbl_Cabecario;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ TotemMuseu/src/totemmuseu/frmQuestionario.java
@@ -0,0 +1,183 @@
+
+package totemmuseu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class frmQuestionario extends javax.swing.JDialog {
+      //vetores para armazenar questões e suas devidas respostas
+        String[] questions = {"Q1","Q2","Q3","Q4","Q5"};
+        String[][] answers = {{"1","2","3","4"}, {"5","6","7","8"} , {"9","10","11","12"} 
+        , {"13","14","15","16"} , {"17","18","19","20"}}; //<- Campos vazios pq ainda não temos as questões elaboradas
+        int index = 0, correct = 0;
+        
+
+
+
+    public frmQuestionario(java.awt.Frame parent, boolean modal) {
+        super(parent, modal);
+        initComponents();
+        
+      
+    }
+
+   
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        bnt_Alternativa1 = new javax.swing.JButton();
+        btn_Alternativa2 = new javax.swing.JButton();
+        btn_Alternativa3 = new javax.swing.JButton();
+        btn_Alternativa4 = new javax.swing.JButton();
+        lbl_Questao = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setIconImage(null);
+        setIconImages(null);
+
+        bnt_Alternativa1.setFont(new java.awt.Font("Times New Roman", 0, 36)); // NOI18N
+        bnt_Alternativa1.setText("A");
+        bnt_Alternativa1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bnt_Alternativa1ActionPerformed(evt);
+            }
+        });
+
+        btn_Alternativa2.setFont(new java.awt.Font("Times New Roman", 0, 36)); // NOI18N
+        btn_Alternativa2.setText("B");
+        btn_Alternativa2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_Alternativa2ActionPerformed(evt);
+            }
+        });
+
+        btn_Alternativa3.setFont(new java.awt.Font("Times New Roman", 0, 36)); // NOI18N
+        btn_Alternativa3.setText("C");
+        btn_Alternativa3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_Alternativa3ActionPerformed(evt);
+            }
+        });
+
+        btn_Alternativa4.setFont(new java.awt.Font("Times New Roman", 0, 36)); // NOI18N
+        btn_Alternativa4.setText("D");
+        btn_Alternativa4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_Alternativa4ActionPerformed(evt);
+            }
+        });
+
+        lbl_Questao.setFont(new java.awt.Font("Segoe UI Light", 0, 36)); // NOI18N
+        lbl_Questao.setText("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lbl_Questao, javax.swing.GroupLayout.PREFERRED_SIZE, 856, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(bnt_Alternativa1, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btn_Alternativa3, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 187, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(btn_Alternativa4, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btn_Alternativa2, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(154, 154, 154))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(14, 14, 14)
+                .addComponent(lbl_Questao, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(36, 36, 36)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bnt_Alternativa1, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn_Alternativa2, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(66, 66, 66)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_Alternativa4, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btn_Alternativa3, javax.swing.GroupLayout.PREFERRED_SIZE, 144, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 446, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void bnt_Alternativa1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bnt_Alternativa1ActionPerformed
+        lbl_Questao.setText(questions[index]);
+        bnt_Alternativa1.setText(answers[index] [0]);
+        btn_Alternativa2.setText(answers[index][1]);
+        btn_Alternativa3.setText(answers[index][2]);
+        btn_Alternativa4.setText(answers[index][3]);
+        
+        //auto incremento que faz o texto do campo e dos botões mudar
+        index++;
+        
+    }//GEN-LAST:event_bnt_Alternativa1ActionPerformed
+
+    private void btn_Alternativa2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_Alternativa2ActionPerformed
+        lbl_Questao.setText(questions[index]);
+        bnt_Alternativa1.setText(answers[index] [0]);
+        btn_Alternativa2.setText(answers[index][1]);
+        btn_Alternativa3.setText(answers[index][2]);
+        btn_Alternativa4.setText(answers[index][3]);
+        
+        index++;
+    }//GEN-LAST:event_btn_Alternativa2ActionPerformed
+
+    private void btn_Alternativa3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_Alternativa3ActionPerformed
+        lbl_Questao.setText(questions[index]);
+        bnt_Alternativa1.setText(answers[index] [0]);
+        btn_Alternativa2.setText(answers[index][1]);
+        btn_Alternativa3.setText(answers[index][2]);
+        btn_Alternativa4.setText(answers[index][3]);
+        
+        index++;
+    }//GEN-LAST:event_btn_Alternativa3ActionPerformed
+
+    private void btn_Alternativa4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_Alternativa4ActionPerformed
+        lbl_Questao.setText(questions[index]);
+        bnt_Alternativa1.setText(answers[index] [0]);
+        btn_Alternativa2.setText(answers[index][1]);
+        btn_Alternativa3.setText(answers[index][2]);
+        btn_Alternativa4.setText(answers[index][3]);
+        
+        index++;
+    }//GEN-LAST:event_btn_Alternativa4ActionPerformed
+
+    public static void main(String args[]) {
+       
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                frmQuestionario questionario = new frmQuestionario(new javax.swing.JFrame(), true);
+                questionario.addWindowListener(new java.awt.event.WindowAdapter() {
+                    @Override
+                    public void windowClosing(java.awt.event.WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
+                questionario.setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bnt_Alternativa1;
+    private javax.swing.JButton btn_Alternativa2;
+    private javax.swing.JButton btn_Alternativa3;
+    private javax.swing.JButton btn_Alternativa4;
+    private javax.swing.JLabel lbl_Questao;
+    // End of variables declaration//GEN-END:variables
+
+
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
