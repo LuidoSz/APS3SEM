f476ee8f367bc8b1b4945215465fe2bf10b566ce
OMirandinha <137963450+OMirandinha@users.noreply.github.com>
2024-04-22 11:07:52 -0300

    Implementação de novas funções

--- TotemMuseu/src/totemmuseu/frmPrincipal.java
+++ TotemMuseu/src/totemmuseu/frmPrincipal.java
@@ -7,10 +7,12 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
  
-public abstract class frmPrincipal extends javax.swing.JDialog implements ActionListener{
+public class frmPrincipal extends javax.swing.JDialog {
+
+
 
     
-    frmPrincipal(java.awt.Frame parent, boolean modal) {
+    frmPrincipal (java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
         
@@ -86,10 +88,14 @@ public abstract class frmPrincipal extends javax.swing.JDialog implements Action
 
 
     private void btn_IniciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_IniciarActionPerformed
-
+        
+                frmPrincipal.this.dispose();
+        
                 frmQuestionario questionario = new frmQuestionario(new javax.swing.JFrame(), true);
    
                 questionario.setVisible(true);
+                
+                
                 
 
     
@@ -107,19 +113,18 @@ public abstract class frmPrincipal extends javax.swing.JDialog implements Action
        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                frmPrincipal dialog = new frmPrincipal(new javax.swing.JFrame(), true) {
-                    @Override
+                frmPrincipal principal = new frmPrincipal(new javax.swing.JFrame(), true) {
                     public void actionPerformed(ActionEvent e) {
                         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
                     }
                 };
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                principal.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
                         System.exit(0);
                     }
                 });
-                dialog.setVisible(true);
+                principal.setVisible(true);
             }
         });
     }
