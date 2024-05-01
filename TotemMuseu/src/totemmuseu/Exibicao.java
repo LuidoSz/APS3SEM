251a929c0684e8feb88d14d603a0a1f32e3ce293
OMirandinha <137963450+OMirandinha@users.noreply.github.com>
2024-05-01 17:00:37 -0300

    Add files via upload

---
+++ TotemMuseu/src/totemmuseu/Exibicao.java
@@ -0,0 +1,22 @@
+package exibicao;
+
+
+import totemmuseu.frmPrincipal;
+import metodos.Resolucao;
+
+public class Exibicao {
+    
+    public static void main(String[] args){
+    frmPrincipal formP = new frmPrincipal(null, true);
+    formP.setLocationRelativeTo(null);
+    //Resolucao res = new Resolucao();
+
+    formP.setVisible(true);
+    
+    
+    }
+
+    
+    
+    
+}
